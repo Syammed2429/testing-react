a463975eac924636d5b02e798acbd17a99daf967
Dada Khalandar <kerry.blig12@gmail.com>
2021-10-23 11:27:05 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { MobileManufacturers } from './component/MobileManufactures/MobileManufacturers.jsx';
+import {MobileOS} from './component/MobileOS/MobileOS.jsx'
+
+function App() {
+  return (
+    <>
+    <MobileOS />
+    <MobileManufacturers />
+    </>
+    
+    
+  );
+}
+
+export default App;
